6b6c412cc8cabd6d02a6a12438896e38538641de
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-08 01:38:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <header></header>
+  <main></main>
+  <footer></footer>
+</template>
+
+<style lang="scss">
+@use './styles/partials/variables' as *;
+@use './styles/partials/mixins' as *;
+@use './styles/general.scss' as *;
+</style>
