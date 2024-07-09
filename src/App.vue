330bc2293bba73b8cfbd1e4005e10858388093cc
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-07-09 07:47:14 +0200

    feat: top navigation bar added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
-<script setup>
+<script>
+import TopNavigationBar from './components/TopNavigationBar.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    TopNavigationBar,
+  }
+}
 </script>
 
 <template>
-  <header></header>
+  <header>
+    <TopNavigationBar />
+  </header>
   <main></main>
   <footer></footer>
 </template>
 
 <style lang="scss">
-@use './styles/partials/variables' as *;
-@use './styles/partials/mixins' as *;
 @use './styles/general.scss' as *;
+header{
+  padding-top: $hgt-top-bar;
+}
 </style>
---
+++ src/components/TopNavigationBar.vue
@@ -0,0 +1,137 @@
+<script>
+export default {
+    name: 'TopNavigationBar',
+
+    data() {
+        return {
+            navBarLinks: [
+                {
+                    text: 'CHARACTERS',
+                    url: '#',
+                    isSelected: false
+                },
+                {
+                    text: 'COMICS',
+                    url: '#',
+                    isSelected: true
+                },
+                {
+                    text: 'MOVIES',
+                    url: '#',
+                    isSelected: false
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    isSelected: false
+                },
+                {
+                    text: 'GAMES',
+                    url: '#',
+                    isSelected: false
+                },
+                {
+                    text: 'COLLECTIBLES',
+                    url: '#',
+                    isSelected: false
+                },
+                {
+                    text: 'VIDEOS',
+                    url: '#',
+                    isSelected: false
+                },
+                {
+                    text: 'FANS',
+                    url: '#',
+                    isSelected: false
+                },
+                {
+                    text: 'NEWS',
+                    url: '#',
+                    isSelected: false
+                },
+                {
+                    text: 'SHOP',
+                    url: '#',
+                    isSelected: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <nav>
+        <div class="content-box">
+            <a href="#" class="logo">
+                <img src="../assets/dc-logo.png" alt="DC Comics">
+            </a>
+
+            <menu>
+                <li :class="{selected: link.isSelected}" v-for="link in navBarLinks">
+                    <a :href="link.url">
+                        {{ link.text }}
+                    </a>
+                </li>
+            </menu>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    font-weight: 700;
+    color: $clr-text-dk;
+    background-color: $clr-secondary;
+
+    .content-box {
+        @include flex-space-between-center;
+        height: $hgt-top-bar;
+
+        .logo {
+            display: block;
+            height: 5rem;
+
+            img {
+                height: 100%
+            }
+        }
+
+        menu {
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            gap: 2rem;
+
+            li {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+
+                &:hover {
+                    color: $clr-primary;
+                }
+
+                &.selected {
+                    color: $clr-primary;
+                    position: relative;
+
+                    &::after {
+                        content: '';
+                        width: 100%;
+                        height: 5px;
+                        background-color: $clr-primary;
+                        position: absolute;
+                        bottom: 0;
+                        right: 0;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
